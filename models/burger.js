2fe642a668727585d8239ea8decc7a9d30627b57
Rebecca88 <rebecca88oliver@gmail.com>
2020-11-06 15:26:29 -0500

    updating controllers and models

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+// My ORM
+var burger = {
+	all: function (cb) {
+		orm.selectAll("burgers", function (res) {
+			cb(res);
+		});
+	},
+	// cols and value arrays
+	create: function (cols, vals, cb){
+		orm.insertOne("burgers", cols, vals, function(res){
+			cb(res);
+		});
+	},
+	update: function (objColVals, condition, cb) {
+		orm.updateOne("burgers", objColVals, condition, function (res){
+			cb(res);
+		})
+	}
+}
+
+module.exports = burger;
